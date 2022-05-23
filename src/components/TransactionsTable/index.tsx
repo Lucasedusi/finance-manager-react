187dc1e3ae42287bfc70c362084327ca819196b5
lucasedusi <lucasmto.16@hotmail.com>
2022-05-23 11:58:34 -0300

    insert transactions api

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -11,14 +11,14 @@ export function TransactionsTable() {
 	return (
 		<Container>
 			<table>
-				<tr>
-					<th>Título</th>
-					<th>Valor</th>
-					<th>Categoria</th>
-					<th>Data</th>
-				</tr>
-
 				<tbody>
+					<tr>
+						<th>Título</th>
+						<th>Valor</th>
+						<th>Categoria</th>
+						<th>Data</th>
+					</tr>
+
 					<tr>
 						<td>Desenvolvimento de Website</td>
 						<td className="deposit">R$12.000,00</td>
